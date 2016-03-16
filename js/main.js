a62d441c10425c5ba78f733d04cefb63ca0979ea
catherinestreiffer <catherinestreiffer@gmail.com>
2016-03-15 19:20:36 -0500

    I think I fixed the problem with repository creation

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//execute script when window is loaded
+window.onload = function(){
+
+    var container = d3.select("body") //get the <body> element from the DOM
+      .append("svg") //put a new svg in the body
+};
