76d5cbe5ec461f2d0b289815a00d7234c04dc230
catherinestreiffer <catherinestreiffer@gmail.com>
2016-03-20 18:20:52 -0500

    no gits

--- js/main.js
+++
@@ -1,6 +0,0 @@
-//execute script when window is loaded
-window.onload = function(){
-
-    var container = d3.select("body") //get the <body> element from the DOM
-      .append("svg") //put a new svg in the body
-};
